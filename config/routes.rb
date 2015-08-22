1e328e8a081d99f6f2836b0b57a64e528a616709
Adison Wu <adison.wu@gmail.com>
2015-08-22 10:22:47 +0800

    進展

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  Rails.application.routes.draw do 
+    root 'products#index'
+    resources :products, only: :show
+    resource :cart, only: %i[show update] #for single resource, ex personal profile
+    resources :orders, only: %i[new create show]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +59,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+
 end
