3ef4aa1ac561a89524df79edf7f4e9121770d271
Adison Wu <adison.wu@gmail.com>
2015-08-22 11:16:33 +0800

    加說明

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # 指定環境要做
   before_action :set_current_cart, if: ->{ Rails.env.development? }
+  # 讓方法變成 helper, 非 controller 也可以用
   helper_method :current_cart
 
   def current_cart
@@ -12,6 +14,7 @@ class ApplicationController < ActionController::Base
   end
 
 private
+  # 測試用方法
   def set_current_cart
     session[:cart_id] = 1
   end
