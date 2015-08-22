77f0df4cf3441a4212005dc2836b6ee0e4f3e34f
Adison Wu <adison.wu@gmail.com>
2015-08-22 10:38:16 +0800

    更新 資料物件

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ActiveRecord::Base
 	has_many :line_items
 	has_many : products, through: :line_items
+  def total
+    line_items.to_a.sum(&:subtotal)
+    # line_items.to_a.sum{|i| i.subtotal} # 同意寫法
+  end
 end
